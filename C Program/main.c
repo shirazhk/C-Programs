cad86855ba5e3eb02582556aecf833e4d757bcf3
Shiraz Khan <srz29@hotmail.com>
2018-03-06 20:37:18 +0530

    Initial Commit

---
+++ C Program/main.c
@@ -0,0 +1,41 @@
+//
+//  main.c
+//  C Program
+//
+//  Created by Shiraz Hasan Khan on 05/12/17.
+//  Copyright © 2017 Srz studios. All rights reserved.
+//
+
+#include<stdio.h>
+int divcount(int y);
+int dcounter=0;
+int main()
+{
+    int N,counter=0;
+    scanf("%d",&N);
+    for(int k=1;k<=N;k++)
+    {
+        if(N%k==0)
+        {
+            counter+=divcount(k);
+        }
+    }
+    printf("%d",counter);
+    return 0;
+}
+
+int divcount(int y)
+{
+    dcounter=0;
+    while(y!=0)
+    {
+        int digit=y%10;
+        if(digit==3||digit==5||digit==6)
+        {
+            dcounter++;
+        }
+        y=y/10;
+    }
+    return dcounter;
+}
+
