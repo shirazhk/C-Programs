3cadec26ad2e7af1455539baf738bf423e5f511a
Shiraz Khan <srz29@hotmail.com>
2018-03-06 21:37:47 +0530

    Assignment 4.4 NPTEL Special Number (We enter a number. Then we see all its divisors and check if they have digits 3,5 or 6 or combinations. If yes then we count it as one. this goes on for all divisors and finally we print the number of times we encountered these "special" numbers) Official Explanation A number is special if it is possible to remove some digits from it to get a number having 3, 5 or 6 only. For example, 38597 is special since it is possible to remove digits 8, 9, 7 to get 35. You cannot remove all the digits. You can remove digits from left, right or middle. Write a program in C which given a number N, calculate how many divisors of N are special
    Input:
    An integer N
    
    Output:
    Output in a single line the number of divisors which are special.
    
    Constraints:
    1 <= N <= 109
    Example:
    Input:
    15
    
    Output:
    3

--- C Program/main.c
+++ C Program/main.c
@@ -6,48 +6,34 @@
 //  Copyright © 2017 Srz studios. All rights reserved.
 //
 #include<stdio.h>
-int sumprefix(int);
-int sumsuffix(int);
-
-
-int main(){
-    int N;
-    scanf("%d", &N);
-    
-    int A[N];
-    int minimum;
-    int i=0;
-    for(i=0; i<N; i++){
-        scanf("%d", &A[i]);
-    }
-    
-    int i1=0;
-    int i2=0;
-    int sum[N];
-    int lc=1;
-    int j=0;
-    
-    for(j=0; j<N; j++){
-        i1 = sumprefix(A[j]);
-    }
-    
-    for(j=0; j<N; j++){
-        i2 = sumprefix(A[j]);
-    }
-    
-    minimum = A[0];
-    for (j=0; j<N; j++){
-        if (A[j] < minimum ){
-            minimum = A[j];
-            lc = j+1;
+int divcount(int y);
+int dcounter=0;
+int main()
+{
+    int N,counter=0;
+    scanf("%d",&N);
+    for(int k=1;k<=N;k++)
+    {
+        if(N%k==0)
+        {
+            counter+=divcount(k);
         }
     }
-    printf("%d",lc);
+    printf("%d",counter);
     return 0;
 }
-int sumprefix(int i){
-    return i;
-}
-int sumsuffix(int i){
-    return i;
+
+int divcount(int y)
+{
+    dcounter=0;
+    while(y>0)
+    {
+        int digit=y%10;
+        if(digit==3||digit==5||digit==6)
+        {
+            dcounter=1;
+        }
+        y=y/10;
+    }
+    return dcounter;
 }
